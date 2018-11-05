55381668c4a2a5c27177948955abfbb3f3592684
Sam Richard <sam.richard@gmail.com>
2018-11-05 00:29:05 -0700

    withModels has to merge

--- src/hoc/withModels.ts
+++ src/hoc/withModels.ts
@@ -1,6 +1,7 @@
 import { Model } from "@rematch/core";
 import { connect } from "react-redux";
 import pick from "lodash/fp/pick";
+import merge from "lodash/fp/merge";
 
 // TODO: there are types here right?
 export const createWithModels = store => withModels;
@@ -9,7 +10,8 @@ export const withModels = (...models: Array<Model | string>) => {
   const pickModels = pick(models.map(m => m.name || m));
   return connect(
     pickModels,
-    pickModels
+    pickModels,
+    merge
   );
 };
 
